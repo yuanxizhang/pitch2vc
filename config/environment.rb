223f78642b0c7a0b7220baaeb7d6de5809d34afb
yuanxizhang <yuanxi.zhang@gmail.com>
2018-12-12 04:54:21 -0800

    update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-configure :development do
-  set :database, 'sqlite3:db/investors.db'
-end
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
 
 require './app'
